a86599e8425abde11d89b4a35f791d160cb3b7ac
JuStrait <justin.l.strait@gmail.com>
2020-12-19 10:01:39 -0800

    added a global string variable and using substrings to log selections

--- docs/script.js
+++ docs/script.js
@@ -1,22 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var fullString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var selections = [];
+var final = [];
 // Write password to the #password input
 function writePassword() {
-  
-  
     var input = prompt("Input a length between 8 and 128 characters.");
     if (input < 8 || input > 128) {
       alert("Try again");
       return;
     }
-  
     if (input > 8 || input < 128); {
       var qa = confirm("Would you like to include lowercase letters?");
       var qb = confirm("Would you like to include uppercase letters?");
       var qc = confirm("Would you like to include numbers?");
       var qd = confirm("Would you like to include special characters?");
     }
+    if (qa) {
+      selections["lowers"] = fullString.substring(0, 25);
+    }
+    if (qb) {
+      selections["uppers"] = fullString.substring(26, 51);
+    }
+    if (qc) {
+      selections["numbers"] = fullString.substring(52, 61);
+    }
+    if (qd) {
+      selections["special"] = fullString.substring(62);
+    }
+    console.log(selections);
+
+    
+      
     
     
     
@@ -24,20 +39,15 @@ function writePassword() {
   
     
   
-  
+    
   //calls the 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-      
+    
+}
 
-
-  var fullString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-  var lower = fullString.substring(0, 25);
-  var upper = fullString.substring(26, 51);
-  var number = fullString.substring(52, 61);
-  var special = fullString.substring(62);
-  var product = [];
+  
   
  
 
@@ -49,7 +59,7 @@ function writePassword() {
 
 
 
-}
+
 
 
 // Add event listener to generate button
