c839923646e3a731407e0e912a1529e16176b018
JuStrait <justin.l.strait@gmail.com>
2020-12-19 16:05:33 -0800

    condensed code to smallest form excluding comment lines

--- docs/script.js
+++ docs/script.js
@@ -7,38 +7,29 @@ function generatePassword(finalpw) {
   var results = "";
   (finalpw, selections);
   for (var index = 0; index < finalpw; index++) {
-    results += selections.charAt(Math.floor(Math.random() * selections.length));
-  }
-  return results;
-}
+    results += selections.charAt(Math.floor(Math.random() * selections.length));  }
+  return results;}
 //  This prompts user input and defines values based off the input
 function writePassword() {
   selections = "";
   var input = parseInt(prompt("Input a length between 8 and 128 characters."));
   if (input < 8 || input > 128) {
     alert("Try again");
-    return;
-  }
+    return;  }
   if (input >8 || input < 128) {
     var qa = confirm("Would you like to include lowercase letters?");
     var qb = confirm("Would you like to include uppercase letters?");
     var qc = confirm("Would you like to include numbers?");
-    var qd = confirm("Would you like to include special characters?");
-  }
+    var qd = confirm("Would you like to include special characters?");  }
   if (qa) {
-    selections += fullString.substring(0, 26);
-  }
+    selections += fullString.substring(0, 26);  }
   if (qb) {
-    selections += fullString.substring(26, 52);
-  }
+    selections += fullString.substring(26, 52);  }
   if (qc) {
-    selections += fullString.substring(52, 62);
-  }
+    selections += fullString.substring(52, 62);  }
   if (qd) {
-    selections += fullString.substring(62);
-  }
-  (selections);
-  var password = generatePassword(input);
+    selections += fullString.substring(62);  }
+    var password = generatePassword(input);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
